0b2e8e3d6440db21faf528be7ef7ce3ef10989ea
Axl Andrade <axlsandrade@outlook.com>
2022-03-04 18:06:34 -0300

    Fixed ortographic issues

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -25,7 +25,7 @@ public class Board {
 	}
 	
 	public Piece piece(int row, int column) {
-		if (!positionExistis(row, column)) {
+		if (!positionExists(row, column)) {
 			throw new BoardException("Position isn't on the board");
 		}
 		return pieces[row][column];
@@ -46,12 +46,12 @@ public class Board {
 		piece.position = position;
 	}
 	
-	private boolean positionExistis(int row, int column) {
+	private boolean positionExists(int row, int column) {
 		return row >= 0 && row < rows && column >= 0 && column < columns;
 	}
 	
 	public boolean positionExists(Position position) {
-		return positionExistis(position.getRow(), position.getColumn());
+		return positionExists(position.getRow(), position.getColumn());
 	}
 	
 	public boolean thereIsAPiece(Position position) {
